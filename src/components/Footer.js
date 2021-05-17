9339dd8079b1bc0dfb841a8763432749bb2b91fa
dana-younis <younisdana7@gmail.com>
2021-05-17 09:11:58 +0300

    1

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+class Footer extends React.Component{
+    render(){
+        return(
+            <h3>
+                Author: Dana younis
+            </h3>
+        )
+    }
+}
+export default Footer;
