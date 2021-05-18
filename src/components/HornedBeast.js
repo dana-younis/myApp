feb17e5cc14fa8c4f17eda42451f6e501168c77e
dana-younis <younisdana7@gmail.com>
2021-05-18 13:39:58 +0300

    1

--- src/components/HornedBeast.js
+++ src/components/HornedBeast.js
@@ -3,46 +3,43 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Card from 'react-bootstrap/Card';
 
 class HornedBeast extends React.Component {
-constructor (prop){
-    super(prop)
-    this.state={
-        number:0
+    constructor(prop) {
+        super(prop)
+        this.state = {
+            number: 0
+        }
+
+
+
+
+    }
+    counter = () => {
+        this.setState(
+            {
+                number: this.state.number + 1
+            }
+        )
     }
 
 
-
-
-}
-counter=()=>{
-    this.setState(
-        {
-            number:this.state.number+1
-        }
-    )
-}
-
-
     render() {
-        return(
-            <div >
+        return (
+            <div>
                 <Card style={{ width: '18rem' }}>
                     <Card.Body>
                         <Card.Title>{this.props.title}</Card.Title>
-                       
-                        <Card.Img onClick={this.counter}  src={this.props.image_url}/>
+
+                        <Card.Img onClick={this.counter} src={this.props.image_url} />
                         <Card.Text>
-                        {this.props.description}
-                         </Card.Text>
-                         <Card.Text>
-                         favourit👨‍🎤️:{this.state.number}
-                         </Card.Text>
-                        
+                            {this.props.description}
+                        </Card.Text>
+                        <Card.Text>
+                            favourit👨‍🎤️:{this.state.number}
+                        </Card.Text>
+
                     </Card.Body>
                 </Card>
-                {/* <h2>{this.props.title}</h2>
-                <img src={this.props.image_url} alt={this.props.title} title={this.props.title}/>
-       
-                  <p>{this.props.description}</p> */}
+
 
             </div>
 
@@ -50,4 +47,4 @@ counter=()=>{
     }
 }
 
-export default HornedBeast ;
+export default HornedBeast;
